dbde6389829636edca21b3a5376d6550e8400ad3
VishwajeetJadhav99 <skj915321@gmail.com>
2026-07-28 21:10:53 +0530

    v

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-00d2dbb426772b03a"
+  ami           = "ami-0884624fc54d115f3"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0fecd01f792e2c2e8"]
   key_name = "linux-newkey"
