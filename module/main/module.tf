dda7f7df75220a22739ba25d1cc96b3fcb857326
VishwajeetJadhav99 <skj915321@gmail.com>
2026-08-10 13:14:33 +0530

    n

--- module/main/module.tf
+++ module/main/module.tf
@@ -1,9 +1,9 @@
 module "ec2" {
    source = "/home/vishwajeet/terraform9/module/resource/ec2"
-   instamid = ami-035827357e3c7e81
-   intype = t3.micro
-   sg = sg-0fecd01f792e2c2e8
-   key = linux-newkey
+   instamid = "ami-035827357e3c7e81"
+   intype = "t3.micro"
+   sg = "sg-0fecd01f792e2c2e8"
+   key = "linux-newkey"
    insdelprotection = false
 
    ws_subnet = module.vpc.subnet_id
