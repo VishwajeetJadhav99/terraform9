e62c55b3a24727b3d998789077bebcabbbdda16f
VishwajeetJadhav99 <skj915321@gmail.com>
2026-08-10 16:28:50 +0530

    v

--- remote/remote.tf
+++ remote/remote.tf
@@ -8,7 +8,7 @@ terraform {
     backend "s3" {
         bucket = "vj99"
         key = "terraform.tfstate"
-        dynamodb_table = "vj9"
+        use_lockfile = true
         region = "ap-south-1"
         profile = "configs"
         shared_credentials_files = ["/root/.aws/credentials"]
