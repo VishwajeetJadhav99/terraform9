d723671e900110f3e1f6053f63108aedbab267bf
VishwajeetJadhav99 <skj915321@gmail.com>
2026-08-10 12:30:19 +0530

    vc

--- module/main/module.tf
+++ module/main/module.tf
@@ -5,7 +5,8 @@ module "ec2" {
    sg = sg-0fecd01f792e2c2e8
    key = linux-newkey
    insdelprotection = false
-   ws_subnet = module.vpc.subnet_id 
+
+   ws_subnet = module.vpc.subnet_id
 }
 
 module "vpc" {
