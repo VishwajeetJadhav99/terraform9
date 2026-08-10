eef9fa024f99b4aebe66b786b8c3edd9ee912aa5
VishwajeetJadhav99 <skj915321@gmail.com>
2026-08-10 12:54:39 +0530

    b

--- module/main/module.tf
+++ module/main/module.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-   source = "./module/resource/ec2"
+   source = "/home/vishwajeet/terraform9/module/resource/ec2"
    instamid = ami-035827357e3c7e81
    intype = t3.micro
    sg = sg-0fecd01f792e2c2e8
@@ -10,7 +10,7 @@ module "ec2" {
 }
 
 module "vpc" {
-    source = "./module/resource/vpc"
+    source = "/home/vishwajeet/terraform9/module/resource/vpc"
     this_cidr = "10.0.0.0/16"
     "this_subcidr = "10.0.1.0/24"
 
