dec3653f632468cd8198bac143ba363f93a7ecf4
VishwajeetJadhav99 <skj915321@gmail.com>
2026-08-10 13:11:37 +0530

    vf

--- module/main/module.tf
+++ module/main/module.tf
@@ -12,6 +12,6 @@ module "ec2" {
 module "vpc" {
     source = "/home/vishwajeet/terraform9/module/resource/vpc"
     this_cidr = "10.0.0.0/16"
-    "this_subcidr = "10.0.1.0/24"
+    this_subcidr = "10.0.1.0/24"
 
 }
