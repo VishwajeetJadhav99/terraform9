d72dfae4854ea4923c2b8200c775ac4a174b7579
VishwajeetJadhav99 <skj915321@gmail.com>
2026-08-12 16:21:34 +0530

    v

---
+++ provisioners/ec2.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "ap-south-1"
+  profile = "configs"
+}
+
+
+
+resource "aws_instance" "web_server" {
+  ami           = var.instamid
+  instance_type = var.intype
+  vpc_security_group_ids = [var.sg]
+ 
+  key_name = var.key
+
+
+provisioner "file" {
+   source = "sample.txt"
+   destination = "/home/ec2-user/aws"
+}
+
+
+provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ips.txt"
+}
+
+connection {
+   type = "ssh"
+   user = "ec2-user"
+   private_key = file("${path.module}/tf.pem")
+   host = self.public_ip
+
+}
+
+provisioner "remote-exec" {
+   "sudo yum install httpd -y"
+   "sudo systemctl start httpd"
+   "sudo yum update"
+   "sudo yum upgrade"
+   "sudo systemctl enable httpd"
+
+}
+
+
+
+
+
+
+
+}
+
---
+++ provisioners/var.tf
@@ -0,0 +1,19 @@
+variable "instamid" {
+    default = "ami-035827357e3c7e810"
+}
+
+
+variable "intype" {
+    default = "t3.micro"
+}
+
+variable "sg" {
+
+    default = "sg-0fecd01f792e2c2e8"
+}
+
+variable "key" {
+
+    default = "vj-key"
+}
+
